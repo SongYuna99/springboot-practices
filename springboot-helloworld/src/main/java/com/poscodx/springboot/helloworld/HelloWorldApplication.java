03d32b2b7fe9b07e6843da0c6862b305c9d1de28
SongYuna99 <thddbsk7248@gmail.com>
2023-10-10 14:02:02 +0900

    @SpringBootApplication @SpringBootConfiguration

--- springboot-helloworld/src/main/java/com/poscodx/springboot/helloworld/HelloWorldApplication.java
+++ springboot-helloworld/src/main/java/com/poscodx/springboot/helloworld/HelloWorldApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 /**
- * 1. Bootstraping class : Spring Application의 Bootstraping 역할 2. 컨테이너의 설정 클래스
- * 역할 : Configuration Class
+ * 1. Bootstraping class : Spring Application의 Bootstraping 역할 
+ * 2. 컨테이너의 설정 클래스 역할 : Configuration Class
  */
 @SpringBootApplication
 public class HelloWorldApplication {
@@ -15,13 +15,15 @@ public class HelloWorldApplication {
 		/**
 		 * SpringApplication.run(...) 안에서 일어나는 일
 		 * 
-		 * 1. Application Context(Spring Container) 생성 2. 만약 Web Application이라면, Web
-		 * Application 타입을 결정(Spring MVC, Reactive) 3. Annotaion Scanning(Component
-		 * Scanning) + Configuration Class를 통한 Bean 생성, 등록, 와이어링 작업 4. 만약 Web
-		 * Application이고 타입이 Spring MVC이면, - 내장(embeded)된
-		 * 서버(TomcatEmbededServiceServletContainer) 인스턴스 생성 - 서버 인스턴스 웹 어플리케이션 배포(MVC,
-		 * AOP, security, JPA, ... Auto Configuration) - 서버 인스턴스 스타트 5.
-		 * ApplicationRunner 인터페이스를 구현한 Bean을 Application Context 찾아서 실행
+		 * 1. Application Context(Spring Container) 생성 
+		 * 2. 만약 Web Application이라면, Web Application 타입을 결정(Spring MVC, Reactive) 
+		 * 3. Annotaion Scanning(Component Scanning) + Configuration Class를 통한
+		 * 	  Bean 생성, 등록, 와이어링 작업 
+		 * 4. 만약 Web Application이고 타입이 Spring MVC이면, 
+		 * 	  - 내장(embeded)된 서버(TomcatEmbededServiceServletContainer) 인스턴스 생성 
+		 * 	  - 서버 인스턴스 웹 어플리케이션 배포(MVC, AOP, security, JPA, ... Auto Configuration) 
+		 * 	  - 서버 인스턴스 스타트 
+		 * 5. ApplicationRunner 인터페이스를 구현한 Bean을 Application Context 찾아서 실행
 		 */
 
 //		ConfigurableApplicationContext ac = null;
